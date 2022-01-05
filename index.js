c32a98a4541359302750db992eb8dba767575c8b
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2022-01-05 16:23:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+//api endpoint routes 
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const uploadImageRoute = require("./routes/uploadimage");
+//app allow and json conf
+dotenv.config();
+app.use(cors());
+  app.use(express.json());
+  app.options('*', cors());
+
+//mongodb connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("db connection successfully"))
+  .catch((err) => {
+    console.log(err);
+  });
+  
+//add routes 
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/uploadImage", uploadImageRoute);
+
+
+
+//get images
+app.use("/images", express.static(path.join(__dirname, "routes/images")));
+//run at port
+app.listen(process.env.PORT || 3000, () => {
+    console.log("server running at "+ process.env.PORT);
+  });
